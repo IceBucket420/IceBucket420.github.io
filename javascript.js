a4e179325f7d0ad33900d65ea986dd710a607965
Pang Le Xin <s10242102@connect.np.edu.sg>
2023-01-31 18:50:56 +0800

    contact html updates

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function Map1234() { 
+    var mapOpt4 = {  
+        center: new google.maps.LatLng(51.508742,-0.120850),  
+        zoom:9,  
+        mapTypeId: google.maps.MapTypeId.TERRAIN  
+      };
+      var w = new google.maps.Map(document.getElementById("Map"),mapOpt4);  
+}  
