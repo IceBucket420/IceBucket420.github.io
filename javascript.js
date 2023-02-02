54089e1ac98ca6787f0e5481ae0d19167b24933f
Pang Le Xin <s10242102@connect.np.edu.sg>
2023-02-02 21:00:14 +0800

    product details edits

--- javascript.js
+++ javascript.js
@@ -7,22 +7,4 @@ btnSubmit.addEventListener('click', function(e){
   e.preventDefault();
   document.getElementById('userdetails').innerHTML = msg;
  
-})
-
-function increaseCount(a, b) {
-  var input = b.previousElementSibling;
-  var value = parseInt(input.value, 10);
-  value = isNaN(value) ? 0 : value;
-  value++;
-  input.value = value;
-}
-
-function decreaseCount(a, b) {
-  var input = b.nextElementSibling;
-  var value = parseInt(input.value, 10);
-  if (value > 1) {
-    value = isNaN(value) ? 0 : value;
-    value--;
-    input.value = value;
-  }
-}
+});
