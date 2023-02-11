6076f92018abafcb3dfcd724c9195737c4ca22d6
Pang Le Xin <s10242102@connect.np.edu.sg>
2023-02-11 16:53:53 +0800

    js updates and html updates

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,27 @@
 
-var numQues = 5;
 
+
+let btnSubmit = document.getElementById('submitBtn');
+
+var sum=0;
+
+function checkAns(){
+    if(document.getElementByName('first_lego').value=='1949') 
+       sum++;
+    if(document.getElementByName('motto').value=='otbige') 
+       sum++;
+    if(document.getElementByName('pantent').value=='28011958') 
+       sum++;
+    if(document.getElementByName('web').value=='1996') 
+       sum++;
+    if(document.getElementByName('store').value=='sa') 
+       sum++;
+   };
+
+
+btnSubmit.addEventListener('click', function(e){
+
+  e.preventDefault();
+  document.getElementById('results').innerHTML = `Your score is ${sum}`;
+ 
+});
