2f7de0922e0b6bf10452688235eea896cd10b0a9
Pang Le Xin <s10242102@connect.np.edu.sg>
2023-02-13 13:13:13 +0800

    css,html,js changes

--- js/minibar.js
+++ js/minibar.js
@@ -1,26 +1,6 @@
-var loggedIn = false
-
-function checklogin(){
-    if(localStorage.getItem("username")!= null){
-        loggedIn == true;
-    }
-};
 
 $(document).ready(function(){
-if (localStorage.getItem("KEY")!= null){
-    $("#carticon").show();
-    $("profileicon").show();
-    $("signinicon").hide();
-}
-else {
-    $("#carticon").hide();
-    $("#profileicon").hide();
-    $("#signinicon").show();
-}
-});
-
-$(document).ready(function(){
-    if (localStorage.getItem("KEY")== null){
+    if (localStorage.getItem("username")== null){
       $("#carticon").hide();
       $("#profileicon").hide();
       $("#signinicon").show();
