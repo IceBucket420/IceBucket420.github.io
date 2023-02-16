01a36674d3ab5bbb8164996de09e2f246436515e
Pang Le Xin <s10242102@connect.np.edu.sg>
2023-02-16 09:33:33 +0800

    js changes

--- js/getuser.js
+++ js/getuser.js
@@ -2,13 +2,17 @@ $(document).ready(function() {
     var username = localStorage.getItem("username");
     var password = localStorage.getItem("password");
     var email = localStorage.getItem("email");
+    var address = localStorage.getItem("address")
 
     console.log(username);
     console.log(password)
     console.log(email)
+    console.log(address)
 
     $("#username").html(username)
     $("#pf_user").html(username)
     $("#pf_email").html(email)
     $("#pf_pass").html(password)
+    $("#input_address").html(address)
+
 })
