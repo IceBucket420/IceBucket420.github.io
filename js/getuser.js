7d9c3bd01ab70aa21ea348a9dd928ab6b83b40a2
Pang Le Xin <s10242102@connect.np.edu.sg>
2023-02-16 10:08:32 +0800

    javascript updates

--- js/getuser.js
+++ js/getuser.js
@@ -3,15 +3,18 @@ $(document).ready(function() {
     var password = localStorage.getItem("password");
     var email = localStorage.getItem("email");
     var address = localStorage.getItem("address")
+    var points = localStorage.getItem("points")
 
     console.log(username);
-    console.log(password)
-    console.log(email)
-    console.log(address)
+    console.log(password);
+    console.log(email);
+    console.log(address);
+    console.log(points);
 
     $("#username").html(username)
     $("#pf_user").html(username)
     $("#pf_email").html(email)
     $("#pf_pass").html(password)
     $("#input_address").html(address)
+    $("#points").html(points)
 })
